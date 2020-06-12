6c5c5e9aa55f4423682953fe53cf1ea3e7c54c9b
prakhil-tp <prakhil.tp@gmail.com>
2020-06-12 22:16:32 +0530

    tests for axios api calls

--- src/__test__/input.test.js
+++ src/__test__/input.test.js
@@ -26,3 +26,31 @@ test("doesn't throw warnings with expected props", () => {
   const expectedProps = { secretWord: "party" };
   checkProps(Input, expectedProps);
 });
+
+describe("state controlled input field", () => {
+  let mockSetCurrentGuess = jest.fn();
+  let wrapper;
+
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+    wrapper = setup();
+  });
+
+  test("state updates with the value of input box upon change", () => {
+    const inputBox = findByTestAttr(wrapper, "input-box");
+
+    const mockEvent = { target: { value: "train" } };
+    inputBox.simulate("change", mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+
+  test("clear state when button clicks", () => {
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+
+    submitButton.simulate("click", { preventDefault() {} });
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
+  });
+});
---
+++ src/actions/hookActions.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const { REACT_APP_BACKEND } = process.env;
+
+export const getSecretWord = async (setSecretWord) => {
+  const response = await axios.get(REACT_APP_BACKEND);
+  setSecretWord(response.data);
+};
