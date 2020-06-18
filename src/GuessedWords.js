1a4af1a8defce3d17da26072d2b710d273faa24e
prakhil-tp <prakhil.tp@gmail.com>
2020-06-18 15:51:40 +0530

    Add language picker tests

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -1,12 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import languageContext from "./contexts/languageContext";
+import stringModule from "./helpers/strings";
 
 const GuessedWords = (props) => {
+  const language = React.useContext(languageContext);
+
   let contents;
   if (props.guessedWords.length === 0) {
     contents = (
       <span data-test="guessed-instructions">
-        Try to guess the secret word!
+        {stringModule.getStringByLanguage(language, "guessPrompt")}
       </span>
     );
   } else {
@@ -18,12 +22,24 @@ const GuessedWords = (props) => {
     ));
     contents = (
       <div data-test="guessed-words">
-        <h3>Guessed words</h3>
+        <h3>
+          {stringModule.getStringByLanguage(language, "guessedWords")}
+        </h3>
         <table className="table table-sm">
           <thead className="thead-light">
             <tr>
-              <th>Guess</th>
-              <th>Matching Letters</th>
+              <th>
+                {stringModule.getStringByLanguage(
+                  language,
+                  "guessColumnHeader"
+                )}
+              </th>
+              <th>
+                {stringModule.getStringByLanguage(
+                  language,
+                  "matchingLettersColumnHeader"
+                )}
+              </th>
             </tr>
           </thead>
           <tbody>{guessedWordRows}</tbody>
